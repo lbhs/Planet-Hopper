9f9f8d411fa8cefcf05d2c3f508aab8d1020f249
MarcEftimie <53620308+MarcEftimie@users.noreply.github.com>
2020-10-19 15:36:17 -0700

    Added controllers to a ship

---
+++ Assets/ShipController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+
+    float rotationspeed = 50.0f;
+
+    void Update()
+    {
+
+        float vertical = Input.GetAxis("Vertical");
+        GameObject.Find("Cube").GetComponent<Rigidbody>().transform.Rotate(0, 0, -Input.GetAxis("Horizontal")/4);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GameObject.Find("Cube").GetComponent<Rigidbody>().AddForce(transform.up/4);
+        }
+
+    }
+
+}
