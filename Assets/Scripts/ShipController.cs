ac9be826868a17a920e03c4a2be7f933109fe831
MarcEftimie <53620308+MarcEftimie@users.noreply.github.com>
2020-10-19 16:01:49 -0700

    Added random orbitor script

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,15 +7,21 @@ public class ShipController : MonoBehaviour
 
     float rotationspeed = 50.0f;
 
+    private Rigidbody gm;
+
+    private void Start()
+    {
+        gm = GetComponent<Rigidbody>();
+    }
     void Update()
     {
 
         float vertical = Input.GetAxis("Vertical");
-        GameObject.Find("Cube").GetComponent<Rigidbody>().transform.Rotate(0, 0, -Input.GetAxis("Horizontal")/4);
+        gm.GetComponent<Rigidbody>().transform.Rotate(0, 0, -Input.GetAxis("Horizontal")/4);
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            GameObject.Find("Cube").GetComponent<Rigidbody>().AddForce(transform.up/4);
+            gm.GetComponent<Rigidbody>().AddForce(transform.up/4);
         }
 
     }
