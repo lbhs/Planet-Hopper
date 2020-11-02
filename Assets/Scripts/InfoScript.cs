ad7673445fe31225108095f63f641f9a66e7ea4b
Gavin Gee <galvindgee@gmail.com>
2020-11-02 15:40:05 -0800

    small changes to infoscript.cs
    added documentation and score variable

--- Assets/Scripts/InfoScript.cs
+++ Assets/Scripts/InfoScript.cs
@@ -1,34 +1,53 @@
-﻿using System.Collections;
+﻿/**
+ * 
+ * InfoScript.cs
+ * Written by Gavin Gee
+ * 
+ * This script is to be placed on the `Info` Game Object. It will hold a majority
+ * of the valuable information in the game. Currently, it keeps track of:
+ * 
+ * - The Planets that are to be spawned at the beginning of the game.
+ * - The Player's score.
+ * 
+ * TODO: More variables will be kept track of once the Score Event System (SES) 
+ * is in place. This script will work closely with the SES.
+ * 
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InfoScript : MonoBehaviour
 {
-    
-    private bool doMercury = true;
-    private bool doVenus = true;
-    private bool doEarth = true;
-    private bool doMars = true;
-    private bool doJupiter = true;
-    private bool doSaturn = true;
-    private bool doUranus = true;
-    private bool doNeptune = true;
+    // These bools control which planets of the solar system will be spawned.
+    private bool mercuryEnabled = true;
+    private bool venusEnabled = true;
+    private bool earthEnabled = true;
+    private bool marsEnabled = true;
+    private bool jupiterEnabled = true;
+    private bool saturnEnabled = true;
+    private bool uranusEnabled = true;
+    private bool neptuneEnabled = true;
 
+    /* This is the player's score. Currently, it doesn't do anything, but it is
+     * useful to keep track of it here.
+     */
+    private int score = 0;
 
+    // This function starts the game and assigns values to the planet bools based on the user's selections.
     public void StartGame()
     {
-        doMercury = GameObject.Find("Mercury Toggle").GetComponent<PlanetActiveToggle>().isSelected;
-        doVenus = GameObject.Find("Venus Toggle").GetComponent<PlanetActiveToggle>().isSelected;
-        doEarth = GameObject.Find("Earth Toggle").GetComponent<PlanetActiveToggle>().isSelected;
-        doMars = GameObject.Find("Mars Toggle").GetComponent<PlanetActiveToggle>().isSelected;
-        doJupiter = GameObject.Find("Jupiter Toggle").GetComponent<PlanetActiveToggle>().isSelected;
-        doSaturn = GameObject.Find("Saturn Toggle").GetComponent<PlanetActiveToggle>().isSelected;
-        doUranus = GameObject.Find("Uranus Toggle").GetComponent<PlanetActiveToggle>().isSelected;
-        doNeptune = GameObject.Find("Neptune Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        mercuryEnabled = GameObject.Find("Mercury Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        venusEnabled = GameObject.Find("Venus Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        earthEnabled = GameObject.Find("Earth Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        marsEnabled = GameObject.Find("Mars Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        jupiterEnabled = GameObject.Find("Jupiter Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        saturnEnabled = GameObject.Find("Saturn Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        uranusEnabled = GameObject.Find("Uranus Toggle").GetComponent<PlanetActiveToggle>().isSelected;
+        neptuneEnabled = GameObject.Find("Neptune Toggle").GetComponent<PlanetActiveToggle>().isSelected;
 
-        DontDestroyOnLoad(gameObject); // TODO: ?? Come back to me later!!
-        // Load next scene?
+        DontDestroyOnLoad(gameObject);
         SceneManager.LoadScene(1);
     }
 
