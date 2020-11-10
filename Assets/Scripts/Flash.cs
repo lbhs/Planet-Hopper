3bca97666e9ca13afdf9b317e6f50d1c2abe0d9d
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2020-11-09 16:30:33 -0800

    Flash and Main Scene update

--- Assets/Scripts/Flash.cs
+++ Assets/Scripts/Flash.cs
@@ -7,24 +7,67 @@ public class Flash : MonoBehaviour
 {
     private Material M;
     private Color color;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        M = this.GetComponent<MeshRenderer>().material;
+        FadeOut();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FadeOut()
     {
-        M = this.GetComponent<MeshRenderer>().material;
-        color = M.color;
-//        color.a = ;
-        M.color = color;
-//        while color.a != 1
-//        {
- //           color.a = color.a + .1f
- //       }
+        M.color = new Color(1, 1, 0, .9f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .8f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .7f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .6f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .5f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .4f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .3f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .2f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .1f);
+        new WaitForSeconds(1); 
+        M.color = new Color(1, 1, 0, 0);
+        new WaitForSeconds(1);
+    }
 
-        
+    void FadeIn()
+    {
+        M.color = new Color(1, 1, 0, .1f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .2f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .3f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .4f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .5f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .6f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .7f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .8f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, .9f);
+        new WaitForSeconds(1);
+        M.color = new Color(1, 1, 0, 1);
+        new WaitForSeconds(1);
+    }
+
+    void FixedUpdate()
+    {
+        if (M.color == new Color(1, 1, 0, 0))
+            FadeIn();
+        if (M.color == new Color(1, 1, 0, 1))
+            FadeOut();
     }
 }
+
