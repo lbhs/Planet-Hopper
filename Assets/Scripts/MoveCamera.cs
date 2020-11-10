f10877a4df7e9d42cf188e0139f3aa7ffb65f9d7
Gavin Gee <galvindgee@gmail.com>
2020-11-09 16:55:39 -0800

    changes to camera and added (broken) LZs

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -2,18 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCameraToShip : MonoBehaviour
+public class MoveCamera : MonoBehaviour
 {
-    private Transform RocketPosition;
+    // This will always be the position of the rocketship.
+    Transform RocketPosition;
+
+    // This will be the position of whatever the camera is targeting.
+    Transform Target;
+
+    // The main camera
+    Camera Cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        RocketPosition = GameObject.Find("Starship").transform;
+        RocketPosition = gameObject.transform;
+        Target = RocketPosition;
+        Cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(RocketPosition.position.x, RocketPosition.position.y, -20);
+        Cam.transform.position = new Vector3(Target.position.x, Target.position.y, -20);
+    }
+
+    // If the camera enters a Sphere of Influence, it will target that planet... (1/2)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Sphere of Influence")
+        {
+            Debug.Log("SOI Entered.");
+            Target = other.gameObject.transform;
+            Cam.transform.position = new Vector3(Target.position.x, Target.position.y, -20);
+            Target.gameObject.GetComponent<GenerateLandingZones>().OpenLZs();
+        }
+    }
+
+    // ... until that Sphere of Influence is exited. (2/2)
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Sphere of Influence")
+        {
+            Debug.Log("SOI Exited.");
+            Target = RocketPosition;
+        }
     }
 }
