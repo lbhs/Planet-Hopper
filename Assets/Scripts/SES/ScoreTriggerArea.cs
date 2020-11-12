480a860e585e72a6ab4d640c72b83502a536213c
Gavin Gee <galvindgee@gmail.com>
2020-11-12 15:34:22 -0800

    small changes to scripts

--- Assets/Scripts/SES/ScoreTriggerArea.cs
+++ Assets/Scripts/SES/ScoreTriggerArea.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class ScoreTriggerArea : MonoBehaviour
 { 
     public int id;
+    private GameObject ship;
+
+    private void Awake()
+    {
+        ship = GameObject.Find("Starship");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "Starship")
+        if (other == ship)
         {
             Debug.Log("Trigger Entered.");
             ScoreEvents.current.Landing(id);
         }
     }
 
+    // checks if a landing is valid...
+    /*
+     * valid landing needs to:
+     * - 
+     */
+    private void CheckLanding()
+    {
+
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        if (other.name == "Starship")
+        if (other == ship)
         {
             Debug.Log("Trigger Exited.");
             ScoreEvents.current.Leaving(id);
