f7fc529c219cd31073deea233481cd3628cd5f81
Gavin Gee <galvindgee@gmail.com>
2020-11-16 15:35:03 -0800

    updated ScoreTriggerArea.cs
    added a (non-functional) check landing function

--- Assets/Scripts/SES/ScoreTriggerArea.cs
+++ Assets/Scripts/SES/ScoreTriggerArea.cs
@@ -17,6 +17,7 @@ public class ScoreTriggerArea : MonoBehaviour
         if (other == ship)
         {
             Debug.Log("Trigger Entered.");
+            Debug.Log(CheckLanding());
             ScoreEvents.current.Landing(id);
         }
     }
@@ -24,11 +25,28 @@ public class ScoreTriggerArea : MonoBehaviour
     // checks if a landing is valid...
     /*
      * valid landing needs to:
-     * - 
+     * - be under a certain speed ( < 100 for now but this can be adjusted )
+     * - be rotated away from the planet ( angle < 10 )
      */
-    private void CheckLanding()
+    private bool CheckLanding()
     {
+        // checks the speed of the ship
+        //if (ship.GetComponent<Rigidbody>().velocity.magnitude > 100f)
+        //{
+        //    return false;
+        //}
 
+        // calculates the unit circle angle for the line between the ship's center and the planet.
+        Vector3 shipPointer = ship.transform.position - gameObject.transform.position;
+        float shipPointerAngle = Vector3.AngleBetween(Vector3.right, shipPointer);
+
+        // calculates the unit circle angle for the ship's direction.
+        Vector3 shipDirection = ship.transform.up;
+        float shipDirectionAngle = Vector3.AngleBetween(Vector3.right, shipDirection);
+
+        float theta = Mathf.Abs(shipPointerAngle - shipDirectionAngle);
+
+        return theta < 10f;
     }
 
     private void OnTriggerExit(Collider other)
