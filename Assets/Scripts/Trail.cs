5b8589c613d82d3c94c329de3ec20e5dd184fa6b
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2020-11-23 17:04:38 -0800

    trail

---
+++ Assets/Scripts/Trail.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+    public Shader lineShader;
+    public Color linecolor;
+    public GameObject Starship;
+    private bool maketrail;
+
+    void Start()
+    {
+        maketrail = true;
+        StartCoroutine("MakeLine");
+    }
+
+    IEnumerator MakeLine()
+    {
+        while (maketrail == true)
+        {
+            var startpos = Starship.transform.position;
+            yield return new WaitForSeconds(5);
+            var endpos = Starship.transform.position;
+            Vector3[] positionArray = new[] { startpos, endpos };
+            GameObject line = new GameObject();
+            line.AddComponent<LineRenderer>();
+            LineRenderer lr = line.GetComponent<LineRenderer>();
+            lr.material = new Material(lineShader);
+            lr.SetColors(linecolor, linecolor);
+            lr.SetWidth(10, 10);
+            lr.SetPositions(positionArray);
+            UnityEngine.Debug.Log("made line");
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
