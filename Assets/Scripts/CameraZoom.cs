58117504c2d0a8f9e32719c09a87e8c1bb9aca6c
MarcEftimie <53620308+MarcEftimie@users.noreply.github.com>
2021-01-11 16:36:56 -0800

    Camera Zoom

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class CameraZoom : MonoBehaviour
 {
 
-   [SerializeField] private Camera Camera;
-    private float zoom;
-    // Start is called before the first frame update
-    void Start()
-    {
-   
-    }
+    [SerializeField] private Camera cam;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleZoom();
+    }
+
+    private void HandleZoom()
+    {
+        float fov = cam.orthographicSize;
+        fov -= Input.GetAxis("Mouse ScrollWheel") * 15;
+        fov = Mathf.Clamp(fov, 1, 100);
+        cam.orthographicSize = fov;
     }
 }
