570775bf403f42eaabbc0cdb8f7a6bcad33bf64c
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2021-02-08 16:52:48 -0800

    fuel guage

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -28,6 +28,9 @@ public class ShipController : MonoBehaviour
 
     public static ShipController main;
 
+    public Image FuelGuage;
+    public float rotation;
+
     private void Start()
     {
         main = this;
@@ -118,6 +121,8 @@ public class ShipController : MonoBehaviour
     {
         T.text = "Fuel: " + Fuel;
         pitStopText.text = "Your fuel is currently: " + Fuel;
+        rotation = 104.491f - (119.767f * (Fuel / 500f));
+        FuelGuage.transform.rotation = Quaternion.Euler(0, 0, rotation);
     }
 
     public void ImmobilizeShip()
