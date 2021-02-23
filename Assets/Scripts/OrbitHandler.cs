268d58c924ecba95bcae238e07d768bd9eff58d9
Gavin Gee <galvindgee@gmail.com>
2021-02-22 17:11:29 -0800

    Small documentation additions

--- Assets/Scripts/OrbitHandler.cs
+++ Assets/Scripts/OrbitHandler.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+﻿/*
+ * OrbitHandler.cs
+ * Gavin Gee
+ * 
+ * This script is a singleton controller script that will put the ship into orbit
+ * of a planet. InitiateOrbit is called by `OrbitTrigger.cs` after staying within
+ * a planets "Orbit Trigger" for 3 seconds.
+ *
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/Scripts/OrbitTrigger.cs
+++ Assets/Scripts/OrbitTrigger.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+﻿/*
+ * OrbitTrigger.cs
+ * Gavin Gee
+ * 
+ * This script is placed on every "Orbit Trigger" gameobject. The script will
+ * tell `OrbitHandler.cs` to initiate an orbit when the ship has stayed within
+ * the trigger for 3 seconds.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
