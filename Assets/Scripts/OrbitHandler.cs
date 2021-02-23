92d19ea044b0b63f7a9ac206870435b9ed261a96
Gavin Gee <galvindgee@gmail.com>
2021-02-22 17:07:38 -0800

    Added Orbits.unity and started OrbitHandler/OrbitTrigger scripts

---
+++ Assets/Scripts/OrbitHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitHandler : MonoBehaviour
+{
+    public static OrbitHandler main;
+
+    void Start()
+    {
+        main = this;
+    }
+
+    public void InitiateOrbit(int ID)
+    {
+        string debugNameOfPlanet;
+        switch (ID)
+        {
+            case 0: debugNameOfPlanet = "Earth.";
+                break;
+            default: debugNameOfPlanet = "An invalid planet!";
+                break;
+        }
+        Debug.Log("Putting the ship into orbit of " + debugNameOfPlanet);
+    }
+}
---
+++ Assets/Scripts/OrbitTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitTrigger : MonoBehaviour
+{
+    public int triggerID;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine("OrbitTimer");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator OrbitTimer()
+    {
+        yield return new WaitForSeconds(3);
+        OrbitHandler.main.InitiateOrbit(triggerID);
+    }
+}
+
