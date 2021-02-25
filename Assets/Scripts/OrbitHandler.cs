138491ef58453de81bc6d44d1ac72616b40abd3c
Gavin Gee <galvindgee@gmail.com>
2021-02-25 12:40:42 -0800

    Added smoother camera

--- Assets/Scripts/OrbitHandler.cs
+++ Assets/Scripts/OrbitHandler.cs
@@ -27,6 +27,9 @@ public class OrbitHandler : MonoBehaviour
     private float pos;
     private float radius;
 
+    private float tCam = 0;
+    private bool camReachedPlanet = false;
+
     void Start()
     {
         main = this;
@@ -49,20 +52,20 @@ public class OrbitHandler : MonoBehaviour
                 break;
         }
         Debug.Log("Putting the ship into orbit of " + debugNameOfPlanet);
-
-        SetOrbitCamera();
         
-        CalculateCyclePosition();
+        CalculateInitialOrbitPosition();
     }
 
     /* This function calculates the ship's position on the unit circle in radians
      * and sets that value to `pos`. It also finds the `radius` of the ship to
      * the planet.
      */
-    private void CalculateCyclePosition()
+    private void CalculateInitialOrbitPosition()
     {
-        float x = ship.transform.position.x - planet.transform.position.x;
-        float y = ship.transform.position.y - planet.transform.position.y;
+        float x = planet.transform.position.x - ship.transform.position.x; // ship.transform.position.x - planet.transform.position.x;
+        float y = planet.transform.position.y - ship.transform.position.y; // ship.transform.position.y - planet.transform.position.y;
+
+
 
         pos = Mathf.Atan(x/y);
         radius = Vector3.Distance(ship.transform.position, planet.transform.position);
@@ -70,15 +73,36 @@ public class OrbitHandler : MonoBehaviour
         Debug.Log("Ship @ (" + x + ", " + y + ") relative to planet, " + pos + " in Radians.");
     }
 
-    /* This function (attempts) to Lerp the camera to a position over the planet.
-     * WIP.
+    /* This function to interpolates the camera to a position over the planet using
+     * `tCam` as its interpolater.
+     * 
      */
     private void SetOrbitCamera()
     {
+        // interpolate
         Camera.main.transform.position = Vector3.Lerp(
             Camera.main.transform.position,
             new Vector3(planet.transform.position.x, planet.transform.position.y, -10f),
-            camSpeed * Time.deltaTime);
+            tCam);
+
+        // update camera interpolater for next iteration
+        tCam += camSpeed * Time.deltaTime;
+
+        // update `camReachedPlanet` if we reached the planet
+        camReachedPlanet = CheckCameraPosition();
+    }
+
+    /* Checks if the Camera has reached it's destination. 
+     * (If tCam == 1, that means it's done Lerping).
+     */
+    private bool CheckCameraPosition()
+    {
+        if (tCam > 1)
+        {
+            Debug.Log("Done Lerping to Planet.");
+            return true;
+        }
+        return false;
     }
 
     /* The ship's position in the orbit is managed here.
@@ -91,10 +115,21 @@ public class OrbitHandler : MonoBehaviour
         // orbit ??
         else
         {
-            ship.transform.position = new Vector3(
-                planet.transform.position.x + (radius * Mathf.Sin(pos += (orbitSpeed * Time.deltaTime))),
-                planet.transform.position.y + (radius * Mathf.Cos(pos += (orbitSpeed * Time.deltaTime))),
-                0);
+            UpdateOrbitPosition();
         }
+
+        // lerp camera if needed
+        if (!camReachedPlanet)
+        {
+            SetOrbitCamera();
+        }
+    }
+
+    private void UpdateOrbitPosition()
+    {
+        ship.transform.position = new Vector3(
+            planet.transform.position.x + (radius * Mathf.Sin(pos += (orbitSpeed * Time.deltaTime))),
+            planet.transform.position.y + (radius * Mathf.Cos(pos += (orbitSpeed * Time.deltaTime))),
+            0);
     }
 }
